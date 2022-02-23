a9c06df90d11348c20506b5844af75949a5cb76e
Candelario <ing.candelario_tic@outlook.com>
2022-02-23 15:33:10 -0600

    update pagos20

--- .Net Framework 4.5/examples/ejemplo.cfdi40.pagos20/ejemplo.cfdi40.pagos20/Program.cs
+++ .Net Framework 4.5/examples/ejemplo.cfdi40.pagos20/ejemplo.cfdi40.pagos20/Program.cs
@@ -13,18 +13,19 @@ namespace ejemplo.cfdi40.pagos20
     {
         static void Main(string[] args)
         {
-            //1- agregar el componente induxsoft.cfdi.v40.dll
-            //Establecer su clave de licencia
+            /*Establecer su clave de licencia
 
             Comprobante.NIC = "Su NIC (Número de Identificación de Cliente)";
             Comprobante.Licencia = "Su clave de licencia";
+            */
+
             //Establecer la ruta del certificado,clave y contraseña del certificado
             string cerfile = @"..\..\..\..\..\..\Recursos\CSD-Pruebas\RFC-PAC-SC\Personas Fisicas\FIEL_CACX7605101P8_20190528152826\cacx7605101p8.cer"; ;
             string keyfile = @"..\..\..\..\..\..\Recursos\CSD-Pruebas\RFC-PAC-SC\Personas Fisicas\FIEL_CACX7605101P8_20190528152826\Claveprivada_FIEL_CACX7605101P8_20190528_152826.key"; ;
             string cerpwd = "12345678a";
 
             // Establecer ubicación de los XSLT del SAT
-            string xslt= @"..\..\..\..\..\..\Recursos\xslt\cadenaoriginal.xslt";
+            string xslt = @"..\..\..\..\..\..\Recursos\xslt\cadenaoriginal.xslt";
             Comprobante.XSLT_CadenaOriginal = xslt;
 
             try
@@ -41,14 +42,14 @@ namespace ejemplo.cfdi40.pagos20
                 //Llenar datos del CFDI
                 cfdi.Serie = "F";
                 cfdi.Folio = "12948";
-                cfdi.Fecha = DateTime.Now;
-                cfdi.NoCertificado = "00001000000504485894";
-                cfdi.SetAttribute("SubTotal", "0");
                 cfdi.Moneda = "XXX";
+                cfdi.Fecha = DateTime.Now;
+                cfdi.NoCertificado = "30001000000400002335";
+                cfdi.SubTotal=0;
                 cfdi.TipoDeComprobante = "P";
                 cfdi.LugarExpedicion = "29039";
                 cfdi.Exportacion = "01";
-                cfdi.SetAttribute("Total", "0");
+                cfdi.Total=0;
 
                 //Llenar datos del emisor
                 cfdi.Emisor.Rfc = "CACX7605101P8";
@@ -66,30 +67,26 @@ namespace ejemplo.cfdi40.pagos20
                 Concepto c = cfdi.CreateElement<Concepto>();
                 c.ClaveProdServ = "84111506";
                 //de esta forma se agrega valor a los atributos de tipo decimal para no hacer la conversión
-                c.SetAttribute("Cantidad", "1.000000"); 
+                c.Cantidad= 1.000000m;
                 c.ClaveUnidad = "ACT";
                 c.Descripcion = "Pago";
                 c.ObjetoImp = "01";
-                c.SetAttribute("ValorUnitario", "0");
-                c.SetAttribute("Importe", "0");
+                c.ValorUnitario=0;
+                c.Importe=0;
                 cfdi.Conceptos.Add(c); //agregamos el nodo concepto a comprobante
 
                 //se crea el nodo pagos
                 Pagos pagos20 = new Pagos();
-                //agregamos el espacio de nombre de pagos20 con su respectiva schemaLocation
-                pagos20.SetAttribute("xmlns:pago20", "http://www.sat.gob.mx/Pagos20");
-                pagos20.SetAttribute("xsi:schemaLocation", "http://www.sat.gob.mx/Pagos20 http://www.sat.gob.mx/sitio_internet/cfd/Pagos/Pagos20.xsd");
-                
                 //creamos el nodo Totales y agregamos valor a sus atributos
-                pagos20.Totales.SetAttribute("MontoTotalPagos", "1.00");
+                pagos20.Totales.MontoTotalPagos=1.00m;
 
                 //creamos el nodo pago
                 Pago pago = new Pago();
                 pago.MonedaP = "MXN";
                 //colocamos valor a los attributos de pago
-                pago.SetAttribute("TipoCambioP", "1");
+                pago.TipoCambioP=1;
                 pago.NumOperacion = "1033";
-                pago.SetAttribute("Monto", "1.00");
+                pago.Monto=1.00m;
                 pago.FormaDePagoP = "01";
                 pago.FechaPago = DateTime.Now;
 
@@ -97,33 +94,33 @@ namespace ejemplo.cfdi40.pagos20
                 DoctoRelacionado doctoRel = pago.CreateElement<DoctoRelacionado>();
                 //doctoRel.EquivalenciaDR = 1;
                 doctoRel.MonedaDR = "MXN";
-                doctoRel.SetAttribute("EquivalenciaDR", "1");
+                doctoRel.EquivalenciaDR=1;
                 doctoRel.Folio = "01";
-                doctoRel.SetAttribute("ImpSaldoInsoluto", "0");
-                doctoRel.SetAttribute("ImpPagado", "1.00");
-                doctoRel.SetAttribute("ImpSaldoAnt", "1.00");
+                doctoRel.ImpSaldoInsoluto=0;
+                doctoRel.ImpPagado=1.00m;
+                doctoRel.ImpSaldoAnt=1.00m;
                 doctoRel.NumParcialidad = 1;
                 //comprobante relacionado
                 //colocar el uuid del comprobante a relacionar
-                doctoRel.IdDocumento = "";
+                doctoRel.IdDocumento = "XXXX6908719f5911470caae7ed455ae685ed";
                 doctoRel.ObjetoImpDR = "02";
 
                 //estos nodos deben existir solamente si el objetoImpDr es 02 de lo contrario no
                 //agregamos el nodo retencionDr
                 RetencionDR RetencionDr = new RetencionDR();
-                RetencionDr.BaseDR = Convert.ToDecimal("2.0");
+                RetencionDr.BaseDR = 2.0m;
                 RetencionDr.ImpuestoDR = "002";
                 RetencionDr.TipoFactorDR = "Tasa";
-                RetencionDr.TasaOCuotaDR = Convert.ToDecimal("0.16");
-                RetencionDr.ImporteDR = Convert.ToDecimal("0.16");
+                RetencionDr.TasaOCuotaDR = 0.16m;
+                RetencionDr.ImporteDR = 0.16m;
 
                 //Creamos el nodo trasladoDr y sus atributos
                 TrasladoDR TrasladoDr = new TrasladoDR();
-                TrasladoDr.BaseDR = Convert.ToDecimal("2.0");
+                TrasladoDr.BaseDR = 2.0m;
                 TrasladoDr.ImpuestoDR = "002";
                 TrasladoDr.TipoFactorDR = "Tasa";
-                TrasladoDr.TasaOCuotaDR = Convert.ToDecimal("0.16");
-                TrasladoDr.ImporteDR = Convert.ToDecimal("0.16");
+                TrasladoDr.TasaOCuotaDR = 0.16m;
+                TrasladoDr.ImporteDR = 0.16m;
                 //agregamos los impuesto al DoctoRelacionado
                 doctoRel.ImpuestosDR.RetencionesDR.Add(RetencionDr);
                 doctoRel.ImpuestosDR.TrasladosDR.Add(TrasladoDr);
@@ -134,10 +131,19 @@ namespace ejemplo.cfdi40.pagos20
 
                 //agregamos el nodo pagos y sus componentes ya agregados al comprobante cfdi
                 cfdi.Complemento.Add(pagos20);
-                
+
                 //para timbrar solo llamamos al método Timbrar del comprobante
+
+                /*
+                Para únicamente sellar, invoque al método Sellar, requerirá haber indicado una clave de licencia.
+                cfdi.Sellar();
+                */
+                //El método timbrar puede invocarse sin establecer una clave de licencia
                 var res = cfdi.Timbrar();
                 Console.WriteLine("El UUID del comprobante timbrado es: " + res["uuid"].ToString());
+                //guardar el xml
+                string ruta = @"..\..\..\..\..\..\.Net Framework 4.5\examples\ejemplo.cfdi40.pagos20\" + res["uuid"].ToString() + ".xml";
+                System.IO.File.WriteAllText(ruta, Encoding.UTF8.GetString(Convert.FromBase64String(res["xml"].ToString())));
 
             }
             catch (Exception ex)
