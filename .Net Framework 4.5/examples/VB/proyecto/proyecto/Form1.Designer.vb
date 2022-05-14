1dc63705b42cbee1076fb35618e31b36e918f4a9
Candelario <ing.candelario_tic@outlook.com>
2022-05-14 18:47:54 -0500

    vb

---
+++ .Net Framework 4.5/examples/VB/proyecto/proyecto/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(46, 50)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(429, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(46, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(241, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Timbrar el Xml de un comprobante desde archivo:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(394, 76)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Seleccionar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(49, 100)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Timbrar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(49, 178)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox2.Size = New System.Drawing.Size(419, 231)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(393, 416)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Timbrar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(46, 151)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(420, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Timbrar desde una cadena (Pegue el contenido XML de un CFDI en el cuadro de texto" &
+    ")"
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.Black
+        Me.Label3.Location = New System.Drawing.Point(-1, 133)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(509, 10)
+        Me.Label3.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(504, 451)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ .Net Framework 4.5/examples/VB/proyecto/proyecto/Form1.vb
@@ -0,0 +1,75 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim opf As New OpenFileDialog()
+        opf.Filter = "Archivos XML|*.xml|Todos los archivos|*.*"
+
+        If opf.ShowDialog() = DialogResult.OK Then
+            TextBox1.Text = opf.FileName
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim comprobante As New induxsoft.cfdi.v40.Comprobante()
+
+        'Si el XML ya está sellado, solo debe indicar Id y pwd de Cuenta de timbrado
+        comprobante.CuentaTimbradoInduxsoft = "SU ID DE CUENTA DE TIMBRADO INDUXSOFT"
+        comprobante.ContrasenaCuentaTimbradoInduxsoft = "CONTRASEÑA DE LA CUENTA DE TIMBRADO INDUXSOFT"
+
+        'Si el XML NO está sellado, debe indicar también los siguientes:
+        'induxsoft.cfdi.v40.Comprobante.NIC = "SU NIC"
+        'induxsoft.cfdi.v40.Comprobante.Licencia = "SU LICENCIA"
+        'comprobante.UbicacionCertificado = "RUTA DEL ARCHIVO CER"
+        'comprobante.UbicacionClavePrivada = "RUTA DEL ARCHIVO KEY"
+        'comprobante.ContrasenaClavePrivada = "CONTRASEÑA DE LA CLAVE PRIVADA"
+        'induxsoft.cfdi.v40.Comprobante.XSLT_CadenaOriginal = "RUTA DEL ARCHIVO DE LA cadenaoriginal.xslt"
+
+        Try
+            comprobante.Open(TextBox1.Text)
+
+            Dim resultado = comprobante.Timbrar()
+            If Convert.ToBoolean(resultado("success")) Then
+                'Timbrado correctamente
+                Dim xmlTimbrado As String
+
+                xmlTimbrado = resultado("xml").ToString()
+            Else
+                MessageBox.Show(resultado("message").ToString())
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim comprobante As induxsoft.cfdi.v40.Comprobante
+
+        Try
+            comprobante = induxsoft.cfdi.v40.Comprobante.FromXml(TextBox2.Text)
+            'Si el XML ya está sellado, solo debe indicar Id y pwd de Cuenta de timbrado
+            comprobante.CuentaTimbradoInduxsoft = "SU ID DE CUENTA DE TIMBRADO INDUXSOFT"
+            comprobante.ContrasenaCuentaTimbradoInduxsoft = "CONTRASEÑA DE LA CUENTA DE TIMBRADO INDUXSOFT"
+
+            'Si el XML NO está sellado, debe indicar también los siguientes:
+            'induxsoft.cfdi.v40.Comprobante.NIC = "SU NIC"
+            'induxsoft.cfdi.v40.Comprobante.Licencia = "SU LICENCIA"
+            'comprobante.UbicacionCertificado = "RUTA DEL ARCHIVO CER"
+            'comprobante.UbicacionClavePrivada = "RUTA DEL ARCHIVO KEY"
+            'comprobante.ContrasenaClavePrivada = "CONTRASEÑA DE LA CLAVE PRIVADA"
+            'induxsoft.cfdi.v40.Comprobante.XSLT_CadenaOriginal = "RUTA DEL ARCHIVO DE LA cadenaoriginal.xslt"
+
+            Dim resultado = comprobante.Timbrar()
+            If Convert.ToBoolean(resultado("success")) Then
+                'Timbrado correctamente
+                Dim xmlTimbrado As String
+
+                xmlTimbrado = resultado("xml").ToString()
+            Else
+                MessageBox.Show(resultado("message").ToString())
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+End Class
