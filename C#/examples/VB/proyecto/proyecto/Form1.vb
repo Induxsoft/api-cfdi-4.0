d911f008e0ffcbc4613252fe04f7fa548ea68b26
Candelario <candelario.hernandez@induxsoft.net>
2023-04-13 15:45:13 -0600

    Se agregó un nuevo binario para la formación  de constancias de retenciones en proyectos con visual basic y se realizó un ejemplo en el proyecto de vb

--- C#/examples/VB/proyecto/proyecto/Form1.vb
+++ C#/examples/VB/proyecto/proyecto/Form1.vb
@@ -1,6 +1,8 @@
 ﻿
 
 Imports System.Text
+Imports vx = induxsoft.cfdi.v40
+
 
 Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
@@ -14,8 +16,9 @@ Public Class Form1
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Dim comprobante As New induxsoft.cfdi.v40.Comprobante()
 
+        '****************************************************Comprobante******************************
+        Dim comprobante As New induxsoft.cfdi.v40.Comprobante()
         'Si el XML ya está sellado, solo debe indicar Id y pwd de Cuenta de timbrado
         comprobante.CuentaTimbradoInduxsoft = "SU ID DE CUENTA DE TIMBRADO INDUXSOFT"
         comprobante.ContrasenaCuentaTimbradoInduxsoft = "CONTRASEÑA DE LA CUENTA DE TIMBRADO INDUXSOFT"
@@ -32,6 +35,39 @@ Public Class Form1
             comprobante.Open(TextBox1.Text)
 
             Dim resultado = comprobante.Timbrar()
+            If Convert.ToBoolean(resultado("success")) Then
+                'Timbrado correctamente
+                Dim xmlTimbrado As String
+
+                xmlTimbrado = resultado("xml").ToString()
+                'System.IO.File.WriteAllText("Ruta", Encoding.UTF8.GetString(Convert.FromBase64String(xmlTimbrado)))
+            Else
+                MessageBox.Show(resultado("message").ToString())
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+        '**********************Retención**********************************
+        Dim retencion As New induxsoft.cfdi.v40.vb.Retenciones()
+        retencion.Open(TextBox1.Text)
+
+        'Si el XML ya está sellado, solo debe indicar Id y pwd de Cuenta de timbrado
+        retencion.CuentaTimbradoInduxsoft = "SU ID DE CUENTA DE TIMBRADO INDUXSOFT"
+        retencion.ContrasenaCuentaTimbradoInduxsoft = "CONTRASEÑA DE LA CUENTA DE TIMBRADO INDUXSOFT"
+
+        'Si el XML NO está sellado, debe indicar también los siguientes:
+        'induxsoft.cfdi.v40.vb.Retenciones.NIC = "SU NIC"
+        'induxsoft.cfdi.v40.vb.Retenciones.Licencia = "SU LICENCIA"
+        'retencion.UbicacionCertificado = "RUTA DEL ARCHIVO CER"
+        'retencion.UbicacionClavePrivada = "RUTA DEL ARCHIVO KEY"
+        'retencion.ContrasenaClavePrivada = "CONTRASEÑA DE LA CLAVE PRIVADA"
+        'induxsoft.cfdi.v40.vb.Retenciones.XSLT_CadenaOriginal = "RUTA DEL ARCHIVO DE LA cadenaoriginal.xslt"
+
+        Try
+            retencion.Open(TextBox1.Text)
+
+            Dim resultado = retencion.Timbrar()
             If Convert.ToBoolean(resultado("success")) Then
                 'Timbrado correctamente
                 Dim xmlTimbrado As String
